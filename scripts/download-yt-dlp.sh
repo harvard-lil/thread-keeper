369a9de7085cd3ebab9fee7f94896a63f84198b3
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2023-04-21 12:02:31 -0400

    yt-dlp upgrade
    https://github.com/yt-dlp/yt-dlp/releases/tag/2023.03.04

--- scripts/download-yt-dlp.sh
+++ scripts/download-yt-dlp.sh
@@ -1,3 +1,3 @@
-# Pulls yt-dlp (2023.02.17 version) and saves it in `executables`.
-curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2023.02.17/yt-dlp > ../executables/yt-dlp;
+# Pulls yt-dlp (2023.03.04 version) and saves it in `executables`.
+curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2023.03.04/yt-dlp > ../executables/yt-dlp;
 chmod a+x ../executables/yt-dlp;
